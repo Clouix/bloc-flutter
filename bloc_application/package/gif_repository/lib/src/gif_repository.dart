8e8d2afdfd5dc5e6740096e4334a785f70e203ec
Ale Borge <clouix23@gmail.com>
2023-04-19 21:54:52 -0600

    gif_repo complete

--- bloc_application/package/gif_repository/lib/src/gif_repository.dart
+++ bloc_application/package/gif_repository/lib/src/gif_repository.dart
@@ -1,7 +1,23 @@
-/// {@template gif_repository}
-/// My new Flutter package
-/// {@endtemplate}
+import 'package:gif_repository/src/models/gif_model.dart';
+import 'package:gif_service/gif_service.dart';
+
+
 class GifRepository {
-  /// {@macro gif_repository}
-  const GifRepository();
+  GifRepository({GifService? gifService})
+      : _gifService = gifService ?? GifService();
+
+  final GifService _gifService;
+
+  Future<List<GifModel>> getUrls() async {
+    final urls = <GifModel>[];
+    try {
+      final listUrls = await _gifService.fetchGifs();
+      for (final url in listUrls) {
+        urls.add(GifModel(url));
+      }
+      return urls;
+    } catch (_) {
+      throw Exception();
+    }
+  }
 }
